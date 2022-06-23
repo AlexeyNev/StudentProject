3ab29c23195995dffc9bca45447519a8ebc6b52c
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-06-23 15:06:34 +0400

    Типы данных

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,16 +1,26 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
-        saveStudentOrder();
-        scheduleStudentOrder();
-        financeStudentOrder();
+        StudentOrder so = new StudentOrder();
+        so.hFirstName = "Alexey";
+        so.hLastName = "Nevredimov";
+        so.wFirstName = "Alla";
+        so.wLastName = "Nevredimova";
+        StudentOrder so1 = new StudentOrder();
+        so1.hFirstName = "Petr";
+        so1.hLastName = "Marsov";
+        so1.wFirstName = "Alisa";
+        so1.wLastName = "Marsova";
+
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
+        long ans1 = saveStudentOrder(so1);
+        System.out.println(ans1);
     }
-    static void saveStudentOrder() {
-        System.out.println("saveStudentOrder");
-    }
-    static void scheduleStudentOrder() {
-        System.out.println("scheduleStudentOrder");
-    }
-    static void financeStudentOrder() {
-        System.out.println("financeStudentOrder");
+    static long saveStudentOrder(StudentOrder marsik) {
+        long answer = 199;
+        System.out.println("saveStudentOrder: " + marsik.hFirstName);
+        System.out.println("saveStudentOrder: " + marsik.hLastName);
+
+        return answer;
     }
 }

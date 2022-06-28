58f54c62f003f3beeb72a39de4e2a39db893d4cd
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-06-28 19:08:47 +0400

    Конструктор

--- src/edu/studentorder/SaveStudentOrder.java
+++ src/edu/studentorder/SaveStudentOrder.java
@@ -1,4 +1,11 @@
 package edu.studentorder;
+/**
+ * Конструктор - по сути это метод класса,
+ * но эта конструкция обладает неким особенным построением.
+ * Оно не умеет возвращать значение (public Person())
+ * Имеет имя которое полностью совпадает с именем класса
+ *
+ */
 
 import edu.studentorder.domain.other.AdultHuman;
 import edu.studentorder.domain.StudentOrder;
@@ -21,10 +28,11 @@ public class SaveStudentOrder {
     static StudentOrder buildStudentOrder() {
         StudentOrder so = new StudentOrder();
         AdultHuman husband = new AdultHuman();
-        husband.setGivenName("Alexey");
-        husband.setSurName("Nevredimov");
-        husband.setPassportNumber("33");
-        so.setHusband(husband);
+        System.out.println(husband.getSurName());
+//        husband.setGivenName("Alexey");
+//        husband.setSurName("Nevredimov");
+//        husband.setPassportNumber("33");
+//        so.setHusband(husband);
 
         String ans = husband.getPersonString();
         System.out.println(ans);

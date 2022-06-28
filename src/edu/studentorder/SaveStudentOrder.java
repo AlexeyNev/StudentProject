5cf9d0a499d40cbaa0f2bf05fb3bd18b374cc36d
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-06-28 17:36:33 +0400

    Наследование

--- src/edu/studentorder/SaveStudentOrder.java
+++ src/edu/studentorder/SaveStudentOrder.java
@@ -1,22 +1,27 @@
 package edu.studentorder;
 
+import edu.studentorder.domain.AdultHuman;
 import edu.studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Alexey";
-        so.hLastName = "Nevredimov";
-        so.wFirstName = "Alla";
-        so.wLastName = "Nevredimova";
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
     static long saveStudentOrder(StudentOrder marsik) {
         long answer = 199;
-        System.out.println("saveStudentOrder: " + marsik.hLastName);
+        System.out.println("saveStudentOrder: ");
 
         return answer;
+    }
+
+    static StudentOrder buildStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        AdultHuman husband = new AdultHuman();
+        husband.setGivenName("Alexey");
+        so.setHusband(husband);
+        return so;
     }
 }
--- src/edu/studentorder/domain/StudentOrder.java
+++ src/edu/studentorder/domain/StudentOrder.java
@@ -1,8 +1,40 @@
 package edu.studentorder.domain;
 
 public class StudentOrder {
-    public String hFirstName;
-    public String hLastName;
-    public String wFirstName;
-    public String wLastName;
+    private long studentOrderId;
+    private AdultHuman husband;
+    private AdultHuman wife;
+    private Child child;
+
+    public long getStudentOrderId() {
+        return studentOrderId;
+    }
+
+    public void setStudentOrderId(long studentOrderId) {
+        this.studentOrderId = studentOrderId;
+    }
+
+    public AdultHuman getHusband() {
+        return husband;
+    }
+
+    public void setHusband(AdultHuman husband) {
+        this.husband = husband;
+    }
+
+    public AdultHuman getWife() {
+        return wife;
+    }
+
+    public void setWife(AdultHuman wife) {
+        this.wife = wife;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public void setChild(Child child) {
+        this.child = child;
+    }
 }

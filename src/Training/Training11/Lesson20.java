bdac802a26e8ebc77c2fb953e906628bf6f95052
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-07-20 17:41:32 +0400

    метод toString()

--- src/Training/Training11/Lesson20.java
+++ src/Training/Training11/Lesson20.java
@@ -4,11 +4,15 @@ package Training.Training11;
  * Перегрузка методов - это методы с одинаковым названием, но с разными параметрами,
  * т.е методы называются одинаково, но делают разное учитывая параметры
  */
+
+/**
+ * toString - метод чтобы представлять объект в виде строки
+ */
 public class Lesson20 {
     public static void main(String[] args) {
-        Human human = new Human();
-        Human human1 = new Human("второй");
-        Human human2 = new Human("третий", 1);
+        Human human = new Human("Petr", 25);
+
+        System.out.println(human);
     }
 }
 
@@ -16,19 +20,14 @@ class Human {
     private String name;
     private int age;
 
-    public Human() {
-        System.out.println("Привет из первого конструктора");
-    }
-
-    public Human(String name) {
-        System.out.println("Привет из второго конструктора");
-        this.name = name;
-    }
-
     public Human(String name, int age) {
         System.out.println("Привет из третьего конструктора");
         this.name = name;
         this.age = age;
+    }
+
+    public String toString() {
+        return name + " " + age;
     }
 
     public String getName() {

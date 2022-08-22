c7f7ec6df48e11abb2e7fb4164748286eec0155a
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-22 13:05:07 +0400

    0.2. Размер списка и добавление элементов.

---
+++ src/list/AddElement.java
@@ -0,0 +1,13 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddElement {
+    public static boolean addNewElement(List<String> list, String str) {
+        list.add(0,"");
+        List<String> check = new ArrayList<>(list);
+        list.equals(check);
+        return true;
+    }
+}

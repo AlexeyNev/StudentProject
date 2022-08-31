d2f574548c9d104433919d3b68b970a6864f324e
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-08-31 17:43:14 +0400

    0.6. Удаление и замена элементов в списке

---
+++ src/Training/list/RepositionElement.java
@@ -0,0 +1,13 @@
+package Training.list;
+
+import java.util.List;
+
+public class RepositionElement {
+    public static List<String> changePosition(List<String> list, int index) {
+        String a = list.remove(list.size() - 1);
+        if (index < list.size()) {
+            list.set(index, a);
+        }
+        return list;
+    }
+}

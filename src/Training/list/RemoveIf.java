0c9eb6eb07c61f4dd325cef46cb9fc3c5e6d5716
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-09-05 17:08:31 +0400

    1.1. Удаление элементов по условию

---
+++ src/Training/list/RemoveIf.java
@@ -0,0 +1,10 @@
+package Training.list;
+
+import java.util.List;
+
+public class RemoveIf {
+    public static List<String> sortList(List<String> list) {
+        list.removeIf(s -> s.length() < 5);
+        return list;
+    }
+}

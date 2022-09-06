e11cb595bc661f25f70c6031a67103e7a035e320
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-09-06 14:44:09 +0400

    1.5. Арифметическая прогрессия

---
+++ src/Training/list/ArProgression.java
@@ -0,0 +1,19 @@
+//package Training.list;
+//
+//import java.util.List;
+//
+//public class ArProgression {
+//    public static int checkData(List<Integer> data) {
+//        int a = data.get(1);
+//        int sum = 0;
+//        for (int d : data) {
+//            sum += d;
+//            return sum;
+//        }
+//
+//        if (a == (data.get(0) + data.get(2))/2) {
+//            return sum;
+//        }
+//        return 0;
+//    }
+//}

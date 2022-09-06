e0fec2c05b0958ca886de6a08a2bd4e32aadf2d1
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2022-09-06 18:25:52 +0400

    1.5. Арифметическая прогрессия

--- src/Training/list/IteratorForList.java
+++ src/Training/list/IteratorForList.java
@@ -4,6 +4,9 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
+ * import java.util.Iterator;
+ * import java.util.List;
+ *
  * public class IteratorForList {
  *     public static void main(String[] args) {
  *         List<String> list = List.of("one", "two", "three", "four", "five");
